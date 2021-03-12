757aea93257320b5b7cfde3bedc183882ead2ff5
niltor <zpty@outlook.com>
2021-03-12 17:23:37 +0800

    add role

---
+++ GT.Lib/Gov.Entity/Authority.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gov.Entity
+{
+    /// <summary>
+    /// 权限表
+    /// </summary>
+    public class Authority : EntityBase
+    {
+
+    }
+}
---
+++ GT.Lib/Gov.Entity/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Gov.Entity
+{
+    /// <summary>
+    /// 用户角色表
+    /// </summary>
+    public class UserRole
+    {
+        public Account Account { get; set; }
+        public Role Role { get; set; }
+        public Guid AccountId { get; set; }
+        public Guid RoleId { get; set; }
+        public DateTimeOffset CreatedTime { get; set; }
+    }
+}
